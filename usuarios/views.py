080035e912e5e72609be44233eb0d881fbdf5ae1
Bruno Barros <brunobarros2093@gmail.com>
2024-05-11 17:26:14 -0300

    index route

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    context = {
+        'nome_pagina': 'MeauDota Home',
+    }
+    return render(request, 'index.html', context)
